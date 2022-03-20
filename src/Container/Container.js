da53c438d7c601052adb758914edd334498d6cd2
Matin <Matin@LAPTOP-23VIAVEQ>
2022-03-20 12:16:26 +0330

    Add a Simple Animation

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,10 +1,14 @@
-import React,{useState} from "react";
+import React,{useState,useRef,useEffect} from "react";
 import './Container.css';
 import InputBox from'../InputBox/InputBox';
 import ListPanel from'../ListPanel/ListPanel';
 import ListItem from'../ListItem/ListItem';
 const Container = ({children}) => {
     const [data,setData] = useState([]);
+    const containerRef = useRef(null);
+    useEffect(() => {
+        containerRef.current.classList.add('visible')
+    },[])
     const callback = (arg) => {
         setData(arg);
     }
@@ -12,7 +16,7 @@ const Container = ({children}) => {
         <div className="col-container mt-5">
             <div className="row d-flex justify-content-center ">
                 <div className="col-md-6">
-                    <div className="card">
+                    <div className="card" ref={containerRef}>
                     <InputBox parentCallback={callback}>
                     </InputBox>
                     <ListPanel>
